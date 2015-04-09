6bb0b596a7f195881f204f1b4c8bdb2ed4fbca45
Rodney Rodriguez <rodneyxr@gmail.com>
2015-04-08 23:01:09 -0500

    fixed activity not inheriting from BaseActivity

--- app/src/main/java/cs3773/com/eventplanner/activities/CreateAccountActivity.java
+++ app/src/main/java/cs3773/com/eventplanner/activities/CreateAccountActivity.java
@@ -1,18 +1,14 @@
 package cs3773.com.eventplanner.activities;
 
 import android.app.AlertDialog;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import cs3773.com.eventplanner.R;
 
-public class CreateAccountActivity extends ActionBarActivity {
+public class CreateAccountActivity extends BaseActivity {
 
     private EditText mEditTextUsername;
     private EditText mEditTextPassword;
@@ -40,11 +36,17 @@ public class CreateAccountActivity extends ActionBarActivity {
         });
     }
 
-    public void getNewAccountInfo(){
+    @Override
+    protected int getSelfNavDrawerItem() {
+        return NAVDRAWER_ITEM_ACCOUNT_CREATE;
+    }
+
+    public void getNewAccountInfo() {
         username = mEditTextUsername.getText().toString();
         password = mEditTextPassword.getText().toString();
         id = mEditTextId.getText().toString();
     }
+
     public void createAccount() {
         new AlertDialog.Builder(this)
                 .setTitle("Created Account")
@@ -53,26 +55,4 @@ public class CreateAccountActivity extends ActionBarActivity {
                 .show();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_account, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
